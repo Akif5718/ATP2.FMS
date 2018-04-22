d691e78b86619869ee06ff89b05f2a297bd49987
Akif5718 <35718815+Akif5718@users.noreply.github.com>
2018-04-23 01:50:46 +0600

    testing

---
+++ V1/ATP2.FMS.Mvc/Controllers/WorkerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FMS_Web_Framework.Base;
+
+namespace ATP2.FMS.Mvc.Controllers
+{
+    public class WorkerController : BaseController
+    {
+        // GET: Worker
+        public ActionResult WorkerProfile()
+        {
+            return View();
+        }
+        public ActionResult JobHome()
+        {
+            return View();
+        }
+    }
+}
