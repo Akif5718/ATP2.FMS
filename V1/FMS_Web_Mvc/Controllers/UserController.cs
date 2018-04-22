7d2112d82080af3fd3e5041f23cf267e3544737c
Akif5718 <35718815+Akif5718@users.noreply.github.com>
2018-04-22 15:17:33 +0600

    Profile View

--- V1/FMS_Web_Mvc/Controllers/UserController.cs
+++ V1/FMS_Web_Mvc/Controllers/UserController.cs
@@ -198,5 +198,49 @@ namespace FMS_Web_Mvc.Controllers
             return RedirectToAction("OwnerForm");
 
         }
+        public ActionResult OwnerProfile()
+        {
+            var ownerInfo = userDao.GetById(1);
+            var ownerCMP = ownerDao.GetByID(1);
+            Owner ownerVM = new Owner();
+            ownerVM.Balance = ownerInfo.Data.Balance;
+            ownerVM.City = ownerInfo.Data.City;
+            ownerVM.CompanyAddress = ownerCMP.Data.CompanyAddress;
+            ownerVM.CompanyName = ownerCMP.Data.CompanyName;
+            ownerVM.Country = ownerInfo.Data.Country;
+            ownerVM.DateofBrith = ownerInfo.Data.DateofBrith;
+            ownerVM.Email = ownerInfo.Data.Email;
+            ownerVM.FristName = ownerInfo.Data.FristName;
+            ownerVM.LastName = ownerInfo.Data.LastName;
+            ownerVM.JoinDate = ownerInfo.Data.JoinDate;
+            ownerVM.ProPic = ownerInfo.Data.ProPic;
+            ownerVM.State = ownerInfo.Data.State;
+            ownerVM.UserId = ownerInfo.Data.UserId;
+            ownerVM.UserType = ownerInfo.Data.UserType;
+            return View(ownerVM);
+
+
+        }
+        [HttpPost]
+        public ActionResult OwnerProfile(OwnerInfo ownerInfo)
+        {
+
+            try
+            {
+                var result = ownerDao.Save(ownerInfo);
+
+                if (result.HasError)
+                {
+                    ViewBag.Message = result.Message;
+                    return View("RegisterForm", ownerInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return RedirectToAction("OwnerForm");
+
+        }
     }
 }
